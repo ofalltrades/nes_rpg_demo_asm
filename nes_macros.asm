dd2c72d605f348f84c2ad2079495bf6048e19bd8
Jake Johnson <ofalltrades@users.noreply.github.com>
2019-11-16 11:12:51 -0500

    just thinking about asm styling

---
+++ nes_macros.asm
@@ -0,0 +1,35 @@
+
+		mac NesHeader
+		seg header
+		org $7ff0
+.new_mapper	SET {1}	;mapper number
+.nes_prg_banks	SET {2}	;number of 16K PRG banks, change to 2 for NROM256
+.nes_char_banks	SET {3}	;number of 8K CHR banks (0 = RAM)
+.new_mirroring	SET {4}	;0 horizontal, 1 vertical, 8 four screen
+		byte $4e,$45,$53,$1a ; header
+		byte .nes_prg_banks
+		byte .nes_char_banks
+		byte .new_mirroring | (.new_mapper <<4 )
+		byte .new_mapper & $f0
+		byte 0, 0, 0, 0, 0, 0, 0, 0 ; reserved, set to zero
+		seg Code
+		org $8000
+		endm
+        
+        
+        	mac NesInit
+        	sei			;disable IRQs
+        	cld			;decimal mode not supported
+        	ldx #$ff
+        	txs			;set up stack pointer
+        	inx			;increment X to 0
+        	stx PPU_MASK		;disable rendering
+        	stx DMC_FREQ		;disable DMC interrupts
+        	stx PPU_CTRL		;disable NMI interrupts
+		bit PPU_STATUS		;clear VBL flag
+        	bit APU_CHAN_CTRL	;ack DMC IRQ bit 7
+		lda #$40
+		sta APU_FRAME		;disable APU Frame IRQ
+		lda #$0F
+		sta APU_CHAN_CTRL	;disable DMC, enable/init other channels.        
+        	endm
--- nes_rpg_demo_asm.asm
+++ nes_rpg_demo_asm.asm
@@ -1,38 +1,44 @@
+; macros:	FooBarBaz
+; subroutines:	foo_bar_baz
+; defs/consts:	FOO_BAR_BAZ
+; variables:	_foo_bar_baz
+; segs:		foo_bar_baz
 
-
-	include "lib_ppu.asm"
-	include "nes_defs.asm"
-        include "nes_macros.asm"
+		include "lib_ppu.asm"
+		include "nes_defs.asm"
+        	include "nes_macros.asm"
 
 ;;;;; VARIABLES
 
-	seg.u ZERO_PAGE
-	org $0
+		seg.u zero_page
+		org $0
 
 ;;;;; NES CARTRIDGE HEADER
 
-	NES_HEADER 0,2,1,NES_MIRR_HORIZ ; mapper 0, 2 PRGs, 1 CHR
+		NesHeader 0, 2, 1, NES_MIRR_HORIZ ; mapper 0, 2 PRGs, 1 CHR
 
 ;;;;; START OF CODE
 
-Start:	subroutine
-	NES_INIT	; set up stack pointer, turn off PPU
-        jsr WaitSync	; wait for VSYNC
-        jsr ClearRAM	; clear RAM
-        jsr WaitSync	; wait for VSYNC (and PPU warmup)
+start:		subroutine
+		NesInit		; set up stack pointer, turn off PPU
+        	jsr WaitSync	; wait for VSYNC
+       		jsr ClearRAM	; clear RAM
+        	jsr WaitSync	; wait for VSYNC (and PPU warmup)
 
-	lda #$3f	; $3F -> A register
-        ldy #$00	; $00 -> Y register
-        sta PPU_ADDR	; write high byte first
-        sty PPU_ADDR    ; $3F00 -> PPU address
-        lda #$1c	; $1C = light blue color
-        sta PPU_DATA    ; $1C -> PPU data
-        lda #CTRL_NMI
-        sta PPU_CTRL	; enable NMI
-        lda #MASK_COLOR
-        sta PPU_MASK	; enable rendering
+		include "nes_macros.asm"
+
+		lda #$3f	; $3F -> A register
+        	ldy #$00	; $00 -> Y register
+        	sta PPU_ADDR	; write high byte first
+        	sty PPU_ADDR    ; $3F00 -> PPU address
+        	lda #$1c	; $1C = light blue color
+        	sta PPU_DATA    ; $1C -> PPU data
+        	lda #CTRL_NMI
+        	sta PPU_CTRL	; enable NMI
+        	lda #MASK_COLOR
+        	sta PPU_MASK	; enable rendering
 .endless
-	jmp .endless	; endless loop
+		jmp .endless	; endless loop
 
 ;;;;; COMMON SUBROUTINES
 
@@ -40,14 +46,11 @@ Start:	subroutine
 
 ;;;;; INTERRUPT HANDLERS
 
-NMIHandler: subroutine
-	SAVE_REGS
-	RESTORE_REGS
-	rti
+nmi_handler: 	subroutine
+		SAVE_REGS
+		RESTORE_REGS
+		rti
 
 ;;;;; CPU VECTORS
 
-	NES_VECTORS
-
-
-	include "nes_macros.asm"
+		NES_VECTORS
