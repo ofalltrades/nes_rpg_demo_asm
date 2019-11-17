897e7a302b42bc73840fd85de5e20fc18e6d0a53
Jake Johnson <kognizant.kog@gmail.com>
2019-11-17 12:18:12 -0500

    style

---
+++ LICENCE.md
@@ -0,0 +1,10 @@
+# Licensing
+
+## Source Code
+
+All scripts and scene files are distributed under the [MIT license](LICENSE.md).
+Copyright held by Juan Linietsky and Ariel Manzur.
+
+## Assets
+
+All data assets, such as images and audio (in any format, including compressed), are unlicenced and © 2019 Phillip J. Johnson. They can be used to run this project locally but should not be distributed or used in your own projects for any purpose.
--- nes_rpg_demo_asm.asm
+++ nes_rpg_demo_asm.asm
@@ -2,55 +2,55 @@
 ; subroutines:	foo_bar_baz
 ; defs/consts:	FOO_BAR_BAZ
 ; variables:	_foo_bar_baz
-; segs:		foo_bar_baz
+; segs:	foo_bar_baz
 
-		include "lib_ppu.asm"
-		include "nes_defs.asm"
+	include "lib_ppu.asm"
+	include "nes_defs.asm"
         	include "nes_macros.asm"
 
-;;;;; VARIABLES
+;;;;; variables
 
-		seg.u zero_page
-		org $0
+	seg.u zero_page
+	org $0
 
-;;;;; NES CARTRIDGE HEADER
+;;;;; nes cartridge header
 
-		NesHeader 0, 2, 1, NES_MIRR_HORIZ ; mapper 0, 2 PRGs, 1 CHR
+	NesHeader 0, 2, 1, NES_MIRR_HORIZ 		; mapper 0, 2 PRGs, 1 CHR
 
-;;;;; START OF CODE
+;;;;; start of code
 
-start:		subroutine
-		NesInit		; set up stack pointer, turn off PPU
-        	jsr WaitSync	; wait for VSYNC
-       		jsr ClearRAM	; clear RAM
-        	jsr WaitSync	; wait for VSYNC (and PPU warmup)
+start:	subroutine
+	NesInit			; set up stack pointer, turn off PPU
+        	jsr WaitVSync			; wait for VSYNC
+       	jsr ClearRAM			; clear RAM
+        	jsr WaitVSync			; wait for VSYNC (and PPU warmup)
 
-		include "nes_macros.asm"
+	include "nes_macros.asm"
 
-		lda #$3f	; $3F -> A register
-        	ldy #$00	; $00 -> Y register
-        	sta PPU_ADDR	; write high byte first
-        	sty PPU_ADDR    ; $3F00 -> PPU address
-        	lda #$1c	; $1C = light blue color
-        	sta PPU_DATA    ; $1C -> PPU data
+	lda #$3f			; $3F -> A register
+        	ldy #$00			; $00 -> Y register
+        	sta PPU_ADDR			; write high byte first
+        	sty PPU_ADDR    			; $3F00 -> PPU address
+        	lda #$1c			; $1C = light blue color
+        	sta PPU_DATA    			; $1C -> PPU data
         	lda #CTRL_NMI
-        	sta PPU_CTRL	; enable NMI
+        	sta PPU_CTRL			; enable NMI
         	lda #MASK_COLOR
-        	sta PPU_MASK	; enable rendering
-.endless
-		jmp .endless	; endless loop
+        	sta PPU_MASK			; enable rendering
+._
+	jmp ._			; endless loop
 
-;;;;; COMMON SUBROUTINES
+;;;;; common subroutines
 
 	include "lib_ppu.asm"
 
-;;;;; INTERRUPT HANDLERS
+;;;;; interrupt handlers
 
 nmi_handler: 	subroutine
-		SAVE_REGS
-		RESTORE_REGS
-		rti
+	SAVE_REGS
+	RESTORE_REGS
+	rti
 
-;;;;; CPU VECTORS
+;;;;; cpu vectors
 
-		NES_VECTORS
+	NES_VECTORS
