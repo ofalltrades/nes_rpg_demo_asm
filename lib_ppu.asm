b6be32ef5e67825b8c540fc0be5045db5a55eacf
Jake Johnson <kognizant.kog@gmail.com>
2019-12-08 02:07:03 -0500

    ,x not , x

--- lib_ppu.asm
+++ lib_ppu.asm
@@ -8,17 +8,17 @@ clear_ram:	subroutine 			; clear CPU-accessible RAM
 	lda #0			; set A and X to zero
 	tax			; A -> X
 .clear				;
-	sta $0, x			; 0 -> [$0 + X]
+	sta $0,x			; 0 -> [$0 + X]
 	cpx #$fe 			; last 2 bytes of stack?
 	bcs .without_stack			; don't clear stack
-	sta $100, x			; 0 -> [$100 + X]
+	sta $100,x			; 0 -> [$100 + X]
 .without_stack				;
-	sta $200, x			; 0 -> [$200 + X]
-	sta $300, x			; 0 -> [$300 + X]
-	sta $400, x			; 0 -> [$400 + X]
-	sta $500, x			; 0 -> [$500 + X]
-	sta $600, x			; 0 -> [$600 + X]
-	sta $700, x			; 0 -> [$700 + X]
+	sta $200,x			; 0 -> [$200 + X]
+	sta $300,x			; 0 -> [$300 + X]
+	sta $400,x			; 0 -> [$400 + X]
+	sta $500,x			; 0 -> [$500 + X]
+	sta $600,x			; 0 -> [$600 + X]
+	sta $700,x			; 0 -> [$700 + X]
 	inx 			;
 	bne .clear 			; loop until X = 0
 	rts			;
