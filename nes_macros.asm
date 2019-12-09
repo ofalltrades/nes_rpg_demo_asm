f5cdf56e47c810ca3503c43a100393827ddce080
Jake Johnson <kognizant.kog@gmail.com>
2019-12-09 17:20:53 -0500

    cleanp

--- nes_macros.asm
+++ nes_macros.asm
@@ -4,7 +4,7 @@
 .nes_prg_banks	set {2}    			; num of 16K PRG banks, change to 2 for NROM256
 .nes_chr_banks	set {3}    			; num of 8K CHR banks (0 = RAM)
 .nes_mirroring	set {4}    			; 0 = hoz, 1 = vert, 8 = four screens
-	byte "NES", $1a    			; header: 'N', 'E', 'S', ' '
+	byte "NES", $1a    			; header: $4e, $45, $53, ' '
 	byte .nes_prg_banks
 	byte .nes_chr_banks
 	byte .nes_mirroring | (.nes_mapper << 4)
