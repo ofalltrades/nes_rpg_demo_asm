aba264d115a9cab3dd89f2cce17faf6b2557d378
Jake Johnson <kognizant.kog@gmail.com>
2019-12-26 14:29:18 -0500

    more formatting

--- nes_rpg_demo_asm.asm
+++ nes_rpg_demo_asm.asm
@@ -7,22 +7,22 @@
 
 
 ;------------ variables
-                    seg.u _ZeroPage_			; define a segment for zero page variables
-                    org $0			; start segment at $0
+                    seg.u _ZeroPage_                                            ; define a segment for zero page variables
+                    org $0                                                      ; start segment at $0
 
-_prg_bank	byte			; current PRG ROM bank
-_prg_bank_mode	byte
-_retrace_cycle	byte			; used to wait for VBlank
-_scroll_x	byte			; used during NMI
-_scroll_y	byte			; used during NMI
-_curr_mirr	byte			; current mirroring scheme
+_prg_bank           byte                                                        ; current PRG ROM bank
+_prg_bank_mode      byte
+_retrace_cycle      byte                                                        ; used to wait for VBlank
+_scroll_x           byte                                                        ; used during NMI
+_scroll_y           byte                                                        ; used during NMI
+_curr_mirr          byte                                                        ; current mirroring scheme
 
 
 ;------------ nes cartridge header
-                    seg _Header_			; define segment for NES header
-                    org HEADER_ADDR			; start header at $7FF0, 16 bytes before PRG ROM data
+                    seg _Header_                                                ; define segment for NES header
+                    org HEADER_ADDR                                             ; start header at $7FF0, 16 bytes before PRG ROM data
 
-                    NESHeader 1, 16, 1, 3 		; mapper 1 (MMC1), 16 16K PRG pages (256K), 16 * 8K CHR ROM, hoz mirroring
+                    NESHeader 1, 16, 1, 3                                       ; mapper 1 (MMC1), 16 16K PRG pages (256K), 16 * 8K CHR ROM, hoz mirroring
 
 
 ;------------ banks
@@ -45,7 +45,7 @@ _curr_mirr	byte			; current mirroring scheme
 
 
 ;------------ tile sets
-                    org CHR_ROM_ADDR			; start CHR data after 2 PRG banks specified in header ($8000-$FFFF)
+                    org CHR_ROM_ADDR                                            ; start CHR data after 2 PRG banks specified in header ($8000-$FFFF)
 
                     incbin "jroatch.chr"
                     incbin "jroatch.chr"
